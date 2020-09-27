bf35252706da5b4aa1333ad9978634d5ed3c1619
khanh-hust <noiphet97@gmail.com>
2020-09-27 23:29:05 +0700

    add cpn list

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,9 +1,15 @@
 import home from '../components/home.vue';
+import list from '../components/list.vue';
 
 export const routes = [
   {
     path : '/',
     name : 'homepage',
     component : home
+  },
+  {
+    path : '/list',
+    name : 'list',
+    component : list
   }
 ]
